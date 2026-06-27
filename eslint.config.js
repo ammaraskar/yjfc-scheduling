c74fd79693fe623ee33e4679ce7d78ae367ba92a
Ammar Askar <ammar@ammaraskar.com>
2026-06-27 11:30:27 -0400

    Fix up README and apply recommended lints

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -11,8 +13,10 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs['recommended-latest'],
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
